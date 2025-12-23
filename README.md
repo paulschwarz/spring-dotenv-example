37731c5f3bc32961df611009adb72f10ddaecc7f
Paul Schwarz <paul.schwarz@hilti.com>
2025-12-23 11:40:18 +0800

    Create example project

---
+++ README.md
@@ -0,0 +1,36 @@
+# Example project using Spring-Dotenv
+
+1. Look at build.gradle.kts
+
+```kotlin
+dependencies {
+    implementation(platform("me.paulschwarz:spring-dotenv-bom:5.0.1"))
+    implementation("me.paulschwarz:springboot4-dotenv")
+}
+```
+
+2. Look at resources/application.yml
+
+```yaml
+app:
+  message: ${APP_MESSAGE:Hello from application.yml}
+  repeat: ${APP_REPEAT:4}
+```
+
+3. Look at .env.example
+
+```dotenv
+APP_MESSAGE=Hello from .env
+APP_REPEAT=10
+```
+
+4. Look at .gitignore
+
+Never commit .env to source control. It's not part of your *code*, it's just a local convenience for having environment variables and system properties configured to test and run your application during local development. It is *not* a substitute for environment variables/system properties on a running server!
+
+```gitignore
+### Local only ###
+.env
+```
+
+**Note** that this .env.example does absolutely nothing until you copy/paste it to a file called `.env` which you can place in the root of your project.
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DemoApplication.AppProps.class)
+public class DemoApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @ConfigurationProperties(prefix = "app")
+    public record AppProps(
+            String message,
+            int repeat
+    ) {}
+
+    @Bean
+    CommandLineRunner runner(AppProps props) {
+        return args -> {
+            for (int i = 0; i < props.repeat(); i++) {
+                log.info("app.message = {}", props.message());
+            }
+        };
+    }
+}
